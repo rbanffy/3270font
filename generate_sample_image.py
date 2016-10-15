7efad863ca699f786a782de7ccd28c89097e7f68
Ricardo Banffy <rbanffy@gmail.com>
2016-10-15 14:20:47 +0100

    Draw reference lines

--- generate_sample_image.py
+++ generate_sample_image.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding:utf-8 -*-
 """
 Generate a sample rendering of the base font.
@@ -8,13 +9,17 @@ from PIL import Image, ImageDraw, ImageFont
 SAMPLE_TEXT = u'''ABCDEFHI1234567890Oijl1IS5qt"'$#!@{}[]()<>çéáÁÑÃÏ¡²³¤€¼½¾'''\
               u'''‘’¥×÷ßø«»®␀␍␊␌▶⚓⯒✘✔✼✎␢…⌘⏎⌫⏻⏼➜●ЯЖ'''
 
-img = Image.new("RGBA", (800, 200), (255, 255, 255))
+HEIGHT = 200
+WIDTH = 800
+
+img = Image.new("RGBA", (WIDTH, HEIGHT), (255, 255, 255))
 draw = ImageDraw.Draw(img)
 y = 0
 
 for size in range(10, 50, 5):
     font = ImageFont.truetype('./3270Medium.otf', size=size)
     y += size * .7
-    draw.text((0, y), SAMPLE_TEXT, (0, 0, 0), font=font)
+    draw.line(((0, y-3), (WIDTH, y-3)), (100, 100, 255, 128), 1)
+    draw.text((0, y), SAMPLE_TEXT, (0, 0, 0, 255), font=font)
 
 img.save("3270Medium_sample.png")
