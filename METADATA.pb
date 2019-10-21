96b8949801448dcac66cecd778a772a8874234df
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2019-10-21 12:24:15 -0300

    add METADATA.pb

---
+++ METADATA.pb
@@ -0,0 +1,34 @@
+name: "3270"
+designer: "Ricardo Bánffy"
+license: "OFL"
+category: "SANS_SERIF"
+date_added: "2019-10-21"
+fonts {
+  name: "3270"
+  style: "normal"
+  weight: 200
+  filename: "3270-ExtraLight.ttf"
+  post_script_name: "3270-ExtraLight"
+  full_name: "3270 ExtraLight"
+  copyright: "Copyright 2019 The 3270 Project Authors (https://github.com/rbanffy/3270)"
+}
+fonts {
+  name: "3270"
+  style: "normal"
+  weight: 300
+  filename: "3270-Light.ttf"
+  post_script_name: "3270-Light"
+  full_name: "3270 Light"
+  copyright: "Copyright 2019 The 3270 Project Authors (https://github.com/rbanffy/3270)"
+}
+fonts {
+  name: "3270"
+  style: "normal"
+  weight: 500
+  filename: "3270-Medium.ttf"
+  post_script_name: "3270-Medium"
+  full_name: "3270 Medium"
+  copyright: "Copyright 2019 The 3270 Project Authors (https://github.com/rbanffy/3270)"
+}
+subsets: "latin"
+subsets: "menu"
--- fontbakery_checks.sh
+++ fontbakery_checks.sh
@@ -5,7 +5,7 @@ rm $OUTPUT_DIR -rf
 mkdir $OUTPUT_DIR
 cp build/*.ttf $OUTPUT_DIR
 cp OFL.txt $OUTPUT_DIR
-#cp METADATA.pb $OUTPUT_DIR
+cp METADATA.pb $OUTPUT_DIR
 #cp DESCRIPTION.*.html $OUTPUT_DIR
 
 for font in $OUTPUT_DIR/*.ttf
@@ -19,7 +19,7 @@ rm $OUTPUT_DIR/*-backup-fonttools-prep-gasp.ttf
 
 export OPTIONS="--no-progress"
 export OPTIONS="$OPTIONS --exclude-checkid /check/ftxvalidator" # We lack this on Travis.
-export OPTIONS="$OPTIONS --exclude-checkid /check/metadata" # Comment this out after creating a METADATA.pb file.
+# export OPTIONS="$OPTIONS --exclude-checkid /check/metadata" # Comment this out after creating a METADATA.pb file.
 export OPTIONS="$OPTIONS --exclude-checkid /check/description" # Comment this out after creating a DESCRIPTION.en_us.html file.
 export OPTIONS="$OPTIONS --exclude-checkid /check/varfont" # Comment this out when making a variable font.
 export OPTIONS="$OPTIONS --loglevel INFO --ghmarkdown Fontbakery-check-results.md"
