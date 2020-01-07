00275d737009d00515f0fc0f2b26c88a24b9d0a7
Ricardo Bánffy <rbanffy@gmail.com>
2020-01-07 20:18:12 +0000

    Capture samples to build directory, add upload to S3

--- test_font_rendering.sh
+++ test_font_rendering.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 ./test_font_rendering.py
-sleep 2  # Needed by Konsole to clear the window size  
+sleep 1  # Needed by Konsole to clear the window size
 # Take a screenshot of the currently active window
-gnome-screenshot -w -f $1.png
+gnome-screenshot -w -f ./build/$1.png
