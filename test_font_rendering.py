9e7de7ac4a2bf5c7b83219e950c4097685a754ad
Ricardo Bánffy <ricardo.banffy@workday.com>
2020-01-15 16:06:47 +0000

    Reformat, add a "confusable" secttion

--- test_font_rendering.py
+++ test_font_rendering.py
@@ -3,7 +3,9 @@
 
 import string
 
-printable = ''.join([c for c in string.printable if c not in string.whitespace])
+printable = "".join(
+    [c for c in string.printable if c not in string.whitespace]
+)
 
 boxes = """
 ┌─┬┐╔═╦╗╓─╥╖╒═╤╕
@@ -14,7 +16,14 @@ boxes = """
 
 misc_glyphs = "Other miscellaneous glyphs: ™●⌘⏎⬇⬆✔✼✎✘‘’◢◣◤◥▮◆\n"
 
-apl_set = "The APL set: ⌶⌷⌸⌹⌺⌻⌼⌽⌾⌿⍀⍁⍂⍃⍄⍅⍆⍇⍈⍉⍊⍋⍌⍍⍎⍏⍐⍑⍒⍓⍔⍕⍖⍗⍘⍙⍚⍛⍜⍝⍞⍟⍠⍡⍢⍣⍤⍥⍦⍧⍨⍩⍪⍫⍬⍭⍮⍯⍰⍱⍲⍳⍴⍵⍶⍷⍸⍹⍺\n"
+apl_set = (
+    "The APL set: ⌶⌷⌸⌹⌺⌻⌼⌽⌾⌿⍀⍁⍂⍃⍄⍅⍆⍇⍈⍉⍊⍋⍌⍍⍎⍏⍐⍑⍒⍓⍔⍕⍖⍗⍘⍙⍚⍛⍜⍝⍞⍟⍠⍡⍢⍣⍤⍥⍦⍧⍨⍩⍪⍫⍬⍭⍮⍯⍰⍱"
+    "⍲⍳⍴⍵⍶⍷⍸⍹⍺\n"
+)
+
+confusables = (
+    "Easily confused pairs bh 5S HX 6G AR kx gy Z2 Il 1l 1I OQ CG DO 0O\n"
+)
 
 BOLD = "\x1b[1m"
 ITALIC = "\x1b[3m"
@@ -27,6 +36,7 @@ if __name__ == "__main__":
     print(boxes)
     print(misc_glyphs)
     print(apl_set)
+    print(confusables)
     print(
         BOLD
         + "BOLD "
