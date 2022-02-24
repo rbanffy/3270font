878683598952b02f4a77d5144e2fa555b25c9f0d
Ricardo Bánffy <rbanffy@gmail.com>
2022-02-24 17:24:28 +0000

    Fix Python formatting

--- generate_sample_image.py
+++ generate_sample_image.py
@@ -13,7 +13,11 @@ CONFUSABLES = "bh 5S HX 6G AR kx gy gq Z2 Il 1l 1I OQ CG DO 0O"
 LINE_COLOR = ImageColor.getrgb("#88f")
 TEXT_COLOR = ImageColor.getrgb("black")
 
-FONT_FILES = ("./build/3270-Regular.ttf", "./build/3270-Regular.otf", "./build/3270-Regular.woff")
+FONT_FILES = (
+    "./build/3270-Regular.ttf",
+    "./build/3270-Regular.otf",
+    "./build/3270-Regular.woff",
+)
 
 
 def draw_sample(font_file):
@@ -28,12 +32,10 @@ def draw_sample(font_file):
     y = 0
     for size in range(15, 55, 5):
         sample_font = ImageFont.truetype(font_file, size=size)
-        offset = size * 0.7 
+        offset = size * 0.7
         y += offset
         # Draw the background reference lines. Upper for the alpha ascender
-        draw_b.line(
-            ((0, y + size * 0.2), (WIDTH, y + size * 0.2)), LINE_COLOR, 1
-        )
+        draw_b.line(((0, y + size * 0.2), (WIDTH, y + size * 0.2)), LINE_COLOR, 1)
         # Lower line for the baseline
         draw_b.line(((0, y + offset), (WIDTH, y + offset)), LINE_COLOR, 1)
         # Draw the point size we are using for the text.
@@ -41,7 +43,7 @@ def draw_sample(font_file):
         # Draw the text itself
         draw_f.text((20, y), SAMPLE_TEXT, TEXT_COLOR, font=sample_font)
 
-    draw_f.text((20,480), font_file, TEXT_COLOR, font=label_font)
+    draw_f.text((20, 480), font_file, TEXT_COLOR, font=label_font)
     return Image.alpha_composite(background, foreground)
 
 
@@ -68,5 +70,11 @@ if __name__ == "__main__":
             rt = draw_readability_test(font, radius)
             sample.paste(rt, (0, 250 + 35 * radius))
         samples.append(sample)
-    sample.save("build/3270_sample.gif", format='GIF', append_images=samples,
-                save_all=True, duration=500, loop=0)
+    sample.save(
+        "build/3270_sample.gif",
+        format="GIF",
+        append_images=samples,
+        save_all=True,
+        duration=500,
+        loop=0,
+    )
